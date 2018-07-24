6c2e7f161078413efb7756c06072b2f86ca058b6
Asaf Niv <asafniv@disroot.org>
2018-07-24 13:49:13 +0300

    Update for LineageOS
    Change-Id: I29983ee9fcd16db7af7084400413d81a6e9f9da1

--- lineage.mk
+++ lineage.mk
@@ -2,7 +2,7 @@
 PRODUCT_RELEASE_NAME := gtelwifi
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/gtelwifi/device_gtelwifi.mk)
